22d6bce0c27d28f784f6fc9022c401e2f25a3703
Rick de Jong <rick.de.jong@mollie.com>
2018-10-12 10:55:43 +0200

    Adding to test with right namimg

--- php/tests/mollie/resellerTest.php
+++ php/tests/mollie/resellerTest.php
@@ -132,7 +132,7 @@ class Mollie_ResellerTest extends PHPUnit_Framework_TestCase
 				'password' => '123456',
 				'partner_id_customer' => '555',
 			]],
-            ['getLoginLinkByPartnerId', [
+            ['getLoginLink', [
                 'partner_id_customer' => '555',
                 'redirect_url' => 'https://www.mollie.com/dashboard/settings/profiles',
             ]],
@@ -163,7 +163,7 @@ class Mollie_ResellerTest extends PHPUnit_Framework_TestCase
 			case 'disconnectAccount':
 				$actual_method = 'disconnect-account';
 				break;
-            case 'getLoginLinkByPartnerId':
+            case 'getLoginLink':
                 $actual_method = 'get-login-link';
                 break;
 			default:
